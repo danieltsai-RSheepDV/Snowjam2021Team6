87406eb5f19a51e9ba75044c27b807b4e7dad54b
kmachnickiwork49 <37753492+kmachnickiwork49@users.noreply.github.com>
2021-12-15 21:12:28 -0800

    Added jump functionality to powerup

--- Assets/Scripts/PowerUpScript.cs
+++ Assets/Scripts/PowerUpScript.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpScript : MonoBehaviour
 {
+    private Rigidbody rb;
+
     // Touch = collect
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Player") {
             // Modify game state, usually the player ->
-
+            rb = other.GetComponentInParent<Rigidbody>();
+            rb.AddForce(transform.up * 1000);
             // Done with object
             Destroy(this.gameObject);
         }
