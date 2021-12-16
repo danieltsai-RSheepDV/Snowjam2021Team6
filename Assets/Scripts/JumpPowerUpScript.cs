0e2c0002545e5bd54d8ef43cd3a8fe1d7ee1cfb2
kmachnickiwork49 <37753492+kmachnickiwork49@users.noreply.github.com>
2021-12-15 21:35:35 -0800

    Refactored to JumpPowerUpScript

--- Assets/Scripts/JumpPowerUpScript.cs
+++ Assets/Scripts/JumpPowerUpScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpScript : MonoBehaviour
+public class JumpPowerUpScript : MonoBehaviour
 {
     private Rigidbody rb;
 
