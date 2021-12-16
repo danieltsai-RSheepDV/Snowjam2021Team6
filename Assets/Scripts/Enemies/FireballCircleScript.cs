e7537cd97c335183869337c9e91c25daad55aedc
Kyle M <Work@MacBook-Pro-6.local>
2021-12-16 10:05:21 -0800

    Fireball circular time-based path

--- Assets/Scripts/Enemies/FireballCircleScript.cs
+++ Assets/Scripts/Enemies/FireballCircleScript.cs
@@ -7,10 +7,13 @@ public class FireballCircleScript : MonoBehaviour
 
     [SerializeField] private float meltRate = 0.01f;
     private Rigidbody rb;
+    private float startTime;
 
     // Start is called before the first frame update
     void Start()
     {
+        Time.timeScale = 1;
+        startTime = Time.time;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -20,14 +23,32 @@ public class FireballCircleScript : MonoBehaviour
             // Modify game state, usually the player ->
             PlayerController pc = other.GetComponentInParent<PlayerController>();
             pc.AddRadius(-meltRate);
+/*
+            //Old size-based tiers of volume removal
+            if (pc.getSnowballSize() == PlayerController.SnowballSize.SMALL) {
+                pc.AddVolume(-3f);
+            } else if (pc.getSnowballSize() == PlayerController.SnowballSize.MEDIUM) {
+                pc.AddVolume(-15f);
+            } else {
+                // Large
+                pc.AddVolume(-75f);
+            }
+*/
             // Done with object?
             // Destroy(this.gameObject);
         }
     }
 
     // Update is called once per frame
+    // Move the ball
     void Update()
     {
-        rb.velocity = new Vector3(5f*Mathf.Sin(transform.position.x / 100f), 0f, 0f);
+        float timeElapsed = Time.time - startTime;
+        Vector3 pos = gameObject.transform.position;
+        rb.velocity = new Vector3(10f*Mathf.Sin(timeElapsed / 3f), 8f*Mathf.Sin(timeElapsed / 0.5f), 10f*Mathf.Cos(timeElapsed / 3f));
+        if (pos.y < 2.5f) {
+            pos = new Vector3(pos.x, 2.5f, pos.z);
+            gameObject.transform.position = pos;
+        }
     }
 }
