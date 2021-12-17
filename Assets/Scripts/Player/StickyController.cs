6991ee1c8c42fbe1e7656da926e5790a32e6c6dc
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2021-12-16 17:22:19 -0800

    Objects now pop off

--- Assets/Scripts/Player/StickyController.cs
+++ Assets/Scripts/Player/StickyController.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
 public class StickyController : MonoBehaviour
 {
     private PlayerController playerController;
+    private List<KeyValuePair<StickableObject, float>> objects = new List<KeyValuePair<StickableObject, float>>();
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +30,26 @@ public class StickyController : MonoBehaviour
 
             if (playerController.GetRadius() > s.radiusLimit)
             {
-                Destroy(other.rigidbody);
-                Destroy(other.collider);
-                other.transform.parent = transform;
+                s.disableObject();
+                s.transform.parent = transform;
                 playerController.AddGrowthRate(playerController.growthValue * 0.25f);
+                
+                objects.Add(new KeyValuePair<StickableObject, float>(s, playerController.GetRadius()));
+            }
+        }
+    }
+
+    public void dropObjects()
+    {
+        foreach(var val in objects.ToList())
+        {
+            if (playerController.GetRadius() < val.Value - 1)
+            {
+                val.Key.enableObject(playerController.transform);
+                val.Key.transform.parent = null;
+
+                objects.Remove(val);
+                Debug.Log(objects.Count);
             }
         }
     }
--- Assets/Scripts/StickableObject.cs
+++ Assets/Scripts/StickableObject.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class StickableObject : MonoBehaviour
 {
     public float radiusLimit;
     public float volume;
+
+    private PlayerController playerController;
     
     // Start is called before the first frame update
     void Start()
@@ -17,5 +21,21 @@ public class StickableObject : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void disableObject()
+    {
+        Destroy(GetComponent<Rigidbody>());
+        gameObject.GetComponent<Collider>().enabled = false;
+    }
+    
+    public void enableObject(Transform t)
+    {
+        gameObject.AddComponent<Rigidbody>();
+        gameObject.GetComponent<Collider>().enabled = true;
+        gameObject.transform.position = t.position + Vector3.up * gameObject.transform.localPosition.magnitude * 2;
+        
+        double d = Random.Range(0f, 360f);
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3((float) Math.Cos(d), 1f, (float) Math.Sin(d)) * gameObject.transform.localPosition.magnitude;
     }
 }
