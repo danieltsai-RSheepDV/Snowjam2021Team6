23d4660c9d3f273ab5f28d9a43c4cbdb61073b70
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-12-17 13:51:39 -0800

    Power Up SFX

--- Assets/Scripts/PowerUps/FattenPowerUpScript.cs
+++ Assets/Scripts/PowerUps/FattenPowerUpScript.cs
@@ -11,6 +11,7 @@ public class FattenPowerUpScript : MonoBehaviour
         if (other.gameObject.CompareTag("Player")) {
             // Modify game state, usually the player ->
             other.GetComponentInParent<PlayerController>().AddRadius(5f);
+            other.transform.parent.GetComponentInChildren<SnowballSound>().PlayPowerUpSFX();
             GameObject label = GameObject.Find("FattenPowerUpText" + id.ToString());
             if (label != null) {
                 Destroy(label);
