649e6403444d7aec4c3b4b02197e94a38a0d0006
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-12-17 14:09:54 -0800

    SoundManagerSingleton

---
+++ Assets/Scripts/SoundManagerSingleton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerSingleton : MonoBehaviour
+{
+    static SoundManagerSingleton instance;
+
+    FMOD.Studio.Bus musicBus;
+    FMOD.Studio.Bus sfxBus;
+    FMOD.Studio.Bus uiBus;
+
+    void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Music");
+        sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
+        uiBus = FMODUnity.RuntimeManager.GetBus("bus:/UI");
+    }
+}
