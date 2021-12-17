f6ed8741e713a0f20db7da9dcbbd2cb461b06207
Kyle M <Work@MacBook-Pro-6.local>
2021-12-17 09:02:15 -0800

    Added bread spawner powerup

--- Assets/Scripts/PowerUps/SpawnerScript.cs
+++ Assets/Scripts/PowerUps/SpawnerScript.cs
@@ -2,30 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossFightScript : MonoBehaviour
+public class SpawnerScript : MonoBehaviour
 {
 
     // Used as a "tag"
-    [SerializeField] private float bossNum = 1;
+    [SerializeField] private float spawnNum = 1;
     string childBaseString = "";
 
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1;
+        childBaseString += "S";
+        childBaseString += spawnNum.ToString();
         childBaseString += "B";
-        childBaseString += bossNum.ToString();
-        childBaseString += "FB";
     }
     private void OnTriggerEnter(Collider other) {
         
         if (other.gameObject.CompareTag("Player")) {
             // Modify game state, usually the player ->
             for (int i = 1; i < 100; i++) {
-                GameObject targetFB = GameObject.Find(childBaseString + i.ToString());
-                if (targetFB != null) {
-                    targetFB.GetComponent<MeshRenderer>().enabled = true;
-                    targetFB.GetComponent<SphereCollider>().enabled = true;
+                GameObject targetB = GameObject.Find(childBaseString + i.ToString());
+                if (targetB != null) {
+                    targetB.GetComponent<MeshRenderer>().enabled = true;
+                    targetB.GetComponent<BoxCollider>().enabled = true;
+                    targetB.GetComponent<Rigidbody>().useGravity = true;
                 }
             }
             // Done with object
