ea101d5db306e9cff469339fb9c3f30d49472d82
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2021-12-16 20:57:01 -0800

    Particles partially done

--- Assets/Scripts/Player/StickyController.cs
+++ Assets/Scripts/Player/StickyController.cs
@@ -9,19 +9,19 @@ public class StickyController : MonoBehaviour
 {
     private PlayerController playerController;
     private List<KeyValuePair<StickableObject, float>> objects = new List<KeyValuePair<StickableObject, float>>();
-
+    
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<PlayerController>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         
     }
-
+    
     void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.GetComponent<StickableObject>() != null)
@@ -38,8 +38,8 @@ public class StickyController : MonoBehaviour
             }
         }
     }
-
-    public void dropObjects()
+    
+    public void DropObjects()
     {
         foreach(var val in objects.ToList())
         {
@@ -47,7 +47,7 @@ public class StickyController : MonoBehaviour
             {
                 val.Key.enableObject(playerController.transform);
                 val.Key.transform.parent = null;
-
+                
                 objects.Remove(val);
                 Debug.Log(objects.Count);
             }
