a980183f96f088cdbf69014f204092ed8409677c
Kyle M <Work@MacBook-Pro-6.local>
2021-12-17 13:13:43 -0800

    Updated fireball sounds, jump script issue

--- Assets/Scripts/PowerUps/JumpPowerUpScript.cs
+++ Assets/Scripts/PowerUps/JumpPowerUpScript.cs
@@ -16,9 +16,9 @@ public class JumpPowerUpScript : MonoBehaviour
             rb = other.GetComponentInParent<Rigidbody>();
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Acceleration);
             // rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, rb.velocity.z);
-            // if (rb.velocity.x < 1f && rb.velocity.z < 1f) {
-            //     rb.velocity = new Vector3(1, jumpVelocity, rb.velocity.z);
-            // }
+            if (rb.velocity.x < 1f && rb.velocity.z < 1f) {
+                rb.velocity = new Vector3(1, rb.velocity.y, rb.velocity.z);
+            }
             // Decision to keep as permanent object
             // Destroy(this.gameObject);
 
